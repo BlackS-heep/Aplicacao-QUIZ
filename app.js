3e724d516ccce36b2bb6b0a2f35359a082fbf152
Nathan-013 <nathan.moreira013@gmail.com>
2022-10-08 16:49:37 -0400

    Quiz v1.1

--- app.js
+++ app.js
@@ -1,32 +1,70 @@
-const form = document.querySelector('.quiz-form')
-const div = document.querySelector('.quiz-body')
-const h1 = document.createElement('h1')
-h1.classList.add('mx-5')
-
 const correctAnswers = ['C', 'A', 'A', 'B']
 
-const quizMessage =  event => {
-  event.preventDefault()
+const form = document.querySelector('.quiz-form')
+const finalScoreContainer = document.querySelector('.final-score-container')
+const redoButton = document.querySelector('.redo-button')
 
-  let score = 0
-  const userAnswers = [
+let score = 0
+
+const getUserAnsewers = () => {
+  return [
     form.inputQuestion1.value,
     form.inputQuestion2.value,
     form.inputQuestion3.value,
     form.inputQuestion4.value
   ]
-
-  const addPointsInScore = (userAnswer, index) => {
-    if(userAnswer === correctAnswers[index]){
-      score += 25
-    }
-  }
-
-  userAnswers.forEach(addPointsInScore)
-
-  const message = `Você acertou ${score}% do quiz`
-  h1.textContent = message
-  div.appendChild(h1)
 }
 
-form.addEventListener('submit', quizMessage)
+const calculateUserScore = userAnswers => {
+  score = 0
+
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
+      score += 25
+    }
+  })
+}
+
+const showFinalResult = () => {
+  scrollTo(0,0)
+  finalScoreContainer.classList.remove('d-none')
+}
+
+const animateFinalResult = () => {
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if (counter === score){
+      clearInterval(timer)
+    }
+
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`
+  }, 20)
+}
+
+const redoQuiz = () => {
+  redoButton.classList.remove('d-none')
+}
+
+redoButton.addEventListener('click', () => {
+  form.button.classList.remove('d-none')
+  redoButton.classList.add('d-none')
+  finalScoreContainer.classList.add('d-none')
+  
+  form.reset()
+})
+
+const handleFormSubmit =  event => {
+  event.preventDefault()
+
+  const userAnswers = getUserAnsewers()
+
+  calculateUserScore(userAnswers)
+  showFinalResult()
+  animateFinalResult()
+
+  form.button.classList.add('d-none')
+  redoQuiz()
+}
+
+form.addEventListener('submit', handleFormSubmit)
